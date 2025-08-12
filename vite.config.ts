e926831a234f79e36e49291f29aaefc968ec5c08
GUSTAVO ANTONIO PERIN <gustavoperin067@gmail.com>
2025-08-11 21:53:20 -0300

    fix: env import

--- vite.config.ts
+++ vite.config.ts
@@ -2,51 +2,57 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import fs from 'fs';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		host: '0.0.0.0',
-		port: 5173,
-		https: {
-			key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
-			cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem'))
-		},
-		proxy: {
-			'/game': {
-				target: process.env.VITE_API_BASE_URL || 'https://chronos.bobagi.click',
-				changeOrigin: true,
-				secure: true
-			}
-		}
-	},
-	test: {
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	console.log('Loaded VITE_API_BASE_URL:', env.VITE_API_BASE_URL);
+
+	return {
+		plugins: [tailwindcss(), sveltekit()],
+		server: {
+			host: '0.0.0.0',
+			port: 5173,
+			https: {
+				key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+				cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem'))
 			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+			proxy: {
+				'/game': {
+					target: env.VITE_API_BASE_URL || 'https://chronos.bobagi.click',
+					changeOrigin: true,
+					secure: true
 				}
 			}
-		]
-	}
+		},
+		test: {
+			projects: [
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'client',
+						environment: 'browser',
+						browser: {
+							enabled: true,
+							provider: 'playwright',
+							instances: [{ browser: 'chromium' }]
+						},
+						include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+						exclude: ['src/lib/server/**'],
+						setupFiles: ['./vitest-setup-client.ts']
+					}
+				},
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'server',
+						environment: 'node',
+						include: ['src/**/*.{test,spec}.{js,ts}'],
+						exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					}
+				}
+			]
+		}
+	};
 });
